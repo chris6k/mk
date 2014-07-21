bebd60c4376f56d05432b2bf3e81a710814e9169
kun.xue <kun.xue@buzzinate.com>
2014-07-21 23:49:58 +0800

    add commission ui

---
+++ src/com/jk/makemoney/CommissionFragment.java
@@ -0,0 +1,25 @@
+package com.jk.makemoney;
+
+import android.view.View;
+
+/**
+ * @author chris.xue
+ */
+public class CommissionFragment extends BasicFragment {
+
+
+    @Override
+    protected void onNavArrowClick(View view) {
+
+    }
+
+    @Override
+    protected void initTitle(View title) {
+
+    }
+
+    @Override
+    protected void initBody(View bodyContainer) {
+
+    }
+}
