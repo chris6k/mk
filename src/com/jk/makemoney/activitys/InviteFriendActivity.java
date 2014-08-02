0951ee71fcc9028b7a121460786513d87ee764da
jgd <kabukon19820702@gmail.com>
2014-08-02 20:43:20 +0800

    fix share

--- src/com/jk/makemoney/activitys/InviteFriendActivity.java
+++ src/com/jk/makemoney/activitys/InviteFriendActivity.java
@@ -1,7 +1,9 @@
 package com.jk.makemoney.activitys;
 
+import android.app.Activity;
 import android.content.ClipData;
 import android.content.ClipboardManager;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,6 +12,12 @@ import android.widget.TextView;
 import android.widget.Toast;
 import com.jk.makemoney.R;
 import com.jk.makemoney.com.jk.makemoney.utils.TextViewUtils;
+import com.umeng.socialize.controller.UMSocialService;
+import com.umeng.socialize.controller.UMServiceFactory;   
+import com.umeng.socialize.media.UMImage;
+import com.umeng.socialize.sso.QZoneSsoHandler;  
+import com.umeng.socialize.sso.SinaSsoHandler;  
+import com.umeng.socialize.sso.TencentWBSsoHandler;
 
 /**
  * @author chris.xue
@@ -38,7 +46,7 @@ public class InviteFriendActivity extends BasicActivity {
     private void initData() {
         TextViewUtils.setText(inviteCount, "100");
         TextViewUtils.setText(totalPaymentText, "1000.0");
-        TextViewUtils.setText(userIdText, "用户ID:1");
+        TextViewUtils.setText(userIdText, "鐢ㄦ埛ID:1");
         TextViewUtils.setText(inviteCodeText, "http://test.com");
         TextViewUtils.setText(inviteChampion, "100");
         TextViewUtils.setText(inviteSecondPlace, "100");
@@ -46,35 +54,37 @@ public class InviteFriendActivity extends BasicActivity {
         TextViewUtils.setText(rewardChampion, "100");
         TextViewUtils.setText(rewardSecondPlace, "100");
         TextViewUtils.setText(rewardThirdPlace, "100");
-        // 首先在您的Activity中添加如下成员变量
+        // 棣栧厛鍦ㄦ偍鐨凙ctivity涓坊鍔犲涓嬫垚鍛樺彉閲�
         final UMSocialService mController = UMServiceFactory.getUMSocialService("com.umeng.share");
-        // 设置分享内容
-        mController.setShareContent("我用赚钱达人给你发红包啦，可以兑换成现金，赶快来领吧！");
-        // 设置分享图片, 参数2为图片的url地址
+        // 璁剧疆鍒嗕韩鍐呭
+        mController.setShareContent("鎴戠敤璧氶挶杈句汉缁欎綘鍙戠孩鍖呭暒锛屽彲浠ュ厬鎹㈡垚鐜伴噾锛岃刀蹇潵棰嗗惂锛�");
+        // 璁剧疆鍒嗕韩鍥剧墖, 鍙傛暟2涓哄浘鐗囩殑url鍦板潃
         mController.setShareMedia(new UMImage(getActivity(), 
                                               "http://www.umeng.com/images/pic/banner_module_social.png"));
         inviteFriendSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(getBaseContext(), "提交", Toast.LENGTH_SHORT).show();
-            }
-            @Override
-            public void onClick(View v) {
-                // 是否只有已登录用户才能打开分享选择页
+                Toast.makeText(getBaseContext(), "鎻愪氦", Toast.LENGTH_SHORT).show();
                 mController.openShare(getActivity(), false);
             }
+
         });
         copyLink.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 ClipboardManager cmb = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
                 cmb.setPrimaryClip(ClipData.newPlainText("inviteCode", ((TextView) inviteCodeText).getText().toString()));
-                Toast.makeText(getBaseContext(), "已复制到剪贴板", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getBaseContext(), "宸插鍒跺埌鍓创鏉�", Toast.LENGTH_SHORT).show();
             }
         });
     }
 
-    private void initComponent() {
+    private Activity getActivity() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private void initComponent() {
         inviteCount = findViewById(R.id.inviteCount);
         totalPaymentText = findViewById(R.id.totalPaymentText);
         userIdText = findViewById(R.id.userIdText);
@@ -96,7 +106,7 @@ public class InviteFriendActivity extends BasicActivity {
 
     @Override
     protected void initTitle(View title) {
-        setTitleBar("朋友奖励");
+        setTitleBar("鏈嬪弸濂栧姳");
     }
 
     @Override
