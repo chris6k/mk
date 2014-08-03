9f12fc0ae95d6505bff9e95d8fe0d9fe51b70075
kun.xue <kun.xue@buzzinate.com>
2014-08-03 22:29:48 +0800

    update encoding

--- src/com/jk/makemoney/activitys/InviteFriendActivity.java
+++ src/com/jk/makemoney/activitys/InviteFriendActivity.java
@@ -1,9 +1,7 @@
 package com.jk.makemoney.activitys;
 
-import android.app.Activity;
 import android.content.ClipData;
 import android.content.ClipboardManager;
-import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,12 +10,10 @@ import android.widget.TextView;
 import android.widget.Toast;
 import com.jk.makemoney.R;
 import com.jk.makemoney.com.jk.makemoney.utils.TextViewUtils;
+import com.umeng.socialize.bean.SHARE_MEDIA;
+import com.umeng.socialize.controller.UMServiceFactory;
 import com.umeng.socialize.controller.UMSocialService;
-import com.umeng.socialize.controller.UMServiceFactory;   
 import com.umeng.socialize.media.UMImage;
-import com.umeng.socialize.sso.QZoneSsoHandler;  
-import com.umeng.socialize.sso.SinaSsoHandler;  
-import com.umeng.socialize.sso.TencentWBSsoHandler;
 
 /**
  * @author chris.xue
@@ -35,6 +31,7 @@ public class InviteFriendActivity extends BasicActivity {
     private View rewardChampion;
     private View rewardSecondPlace;
     private View rewardThirdPlace;
+    private UMSocialService mController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,7 +43,7 @@ public class InviteFriendActivity extends BasicActivity {
     private void initData() {
         TextViewUtils.setText(inviteCount, "100");
         TextViewUtils.setText(totalPaymentText, "1000.0");
-        TextViewUtils.setText(userIdText, "鐢ㄦ埛ID:1");
+        TextViewUtils.setText(userIdText, "用户ID:1");
         TextViewUtils.setText(inviteCodeText, "http://test.com");
         TextViewUtils.setText(inviteChampion, "100");
         TextViewUtils.setText(inviteSecondPlace, "100");
@@ -54,18 +51,23 @@ public class InviteFriendActivity extends BasicActivity {
         TextViewUtils.setText(rewardChampion, "100");
         TextViewUtils.setText(rewardSecondPlace, "100");
         TextViewUtils.setText(rewardThirdPlace, "100");
-        // 棣栧厛鍦ㄦ偍鐨凙ctivity涓坊鍔犲涓嬫垚鍛樺彉閲�
-        final UMSocialService mController = UMServiceFactory.getUMSocialService("com.umeng.share");
-        // 璁剧疆鍒嗕韩鍐呭
-        mController.setShareContent("鎴戠敤璧氶挶杈句汉缁欎綘鍙戠孩鍖呭暒锛屽彲浠ュ厬鎹㈡垚鐜伴噾锛岃刀蹇潵棰嗗惂锛�");
-        // 璁剧疆鍒嗕韩鍥剧墖, 鍙傛暟2涓哄浘鐗囩殑url鍦板潃
-        mController.setShareMedia(new UMImage(getActivity(), 
-                                              "http://www.umeng.com/images/pic/banner_module_social.png"));
+        //
+        mController = UMServiceFactory.getUMSocialService("com.umeng.share");
+        //
+        mController.setShareContent("邀请好友");
+        //
+        mController.getConfig().setShareSms(true);
+        mController.getConfig().setShareMail(true);
+        mController.setShareMedia(new UMImage(InviteFriendActivity.this,
+                "http://www.umeng.com/images/pic/banner_module_social.png"));
+        mController.getConfig().setPlatforms(SHARE_MEDIA.WEIXIN, SHARE_MEDIA.WEIXIN_CIRCLE,
+                SHARE_MEDIA.QQ, SHARE_MEDIA.SINA, SHARE_MEDIA.TENCENT);
+        mController.getConfig().setPlatformOrder(SHARE_MEDIA.WEIXIN, SHARE_MEDIA.WEIXIN_CIRCLE,
+                SHARE_MEDIA.QQ, SHARE_MEDIA.SINA, SHARE_MEDIA.TENCENT);
         inviteFriendSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(getBaseContext(), "鎻愪氦", Toast.LENGTH_SHORT).show();
-                mController.openShare(getActivity(), false);
+                mController.openShare(InviteFriendActivity.this, false);
             }
 
         });
@@ -74,17 +76,12 @@ public class InviteFriendActivity extends BasicActivity {
             public void onClick(View view) {
                 ClipboardManager cmb = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
                 cmb.setPrimaryClip(ClipData.newPlainText("inviteCode", ((TextView) inviteCodeText).getText().toString()));
-                Toast.makeText(getBaseContext(), "宸插鍒跺埌鍓创鏉�", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getBaseContext(), "已经复制到剪贴板", Toast.LENGTH_SHORT).show();
             }
         });
     }
 
-    private Activity getActivity() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	private void initComponent() {
+    private void initComponent() {
         inviteCount = findViewById(R.id.inviteCount);
         totalPaymentText = findViewById(R.id.totalPaymentText);
         userIdText = findViewById(R.id.userIdText);
@@ -106,7 +103,7 @@ public class InviteFriendActivity extends BasicActivity {
 
     @Override
     protected void initTitle(View title) {
-        setTitleBar("鏈嬪弸濂栧姳");
+        setTitleBar("朋友奖励");
     }
 
     @Override
